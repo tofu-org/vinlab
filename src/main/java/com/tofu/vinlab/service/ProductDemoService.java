31d0f1cf2e9d95000b5107be6fd4050ce3100da2
p50000 <rentacat45@gmail.com>
2025-11-11 18:04:39 +0300

    optimize code

--- src/main/java/com/tofu/vinlab/service/ProductDemoService.java
+++ src/main/java/com/tofu/vinlab/service/ProductDemoService.java
@@ -15,11 +15,11 @@ public class ProductDemoService {
      */
     public void demonstrateBuyableInterface(Buyable item) {
         logger.info("=== Демонстрация через интерфейс ===");
-        logger.log(Level.INFO, "Доступно 5 единиц: {0}", item.isAvailable(5));
+        logger.log(Level.INFO, "Доступно 5 единиц: {}", item.isAvailable(5));
 
         try {
             double total = item.buy(1);
-            logger.log(Level.INFO, "Куплено через интерфейс. Стоимость: %.2f %n", total);
+            logger.log(Level.INFO, "Куплено через интерфейс. Стоимость: {} %n", total);
         } catch (IllegalArgumentException e) {
             logger.log(Level.WARNING, "Ошибка покупки через интерфейс: {}", e.getMessage());
         }
@@ -32,15 +32,15 @@ public class ProductDemoService {
      */
     public void demonstrateProduct(Product product) {
         logger.info("=== Демонстрация товара ===");
-        logger.info("Товар: " + product);
-        logger.info("Доступно 3 единицы: " + product.isAvailable(3));
+        logger.log(Level.INFO, "Товар: {}",  product);
+        logger.log(Level.INFO, "Доступно 3 единицы: {}" , product.isAvailable(3));
 
         try {
             double total = product.buy(2);
-            logger.log(Level.INFO, "Куплено 2 единицы. Общая стоимость: %.2f%n", total);
-            logger.log(Level.INFO, "Остаток на складе: %d",  product.getStockQuantity());
+            logger.log(Level.INFO, "Куплено 2 единицы. Общая стоимость: {}%n", total);
+            logger.log(Level.INFO, "Остаток на складе: {}",  product.getStockQuantity());
         } catch (IllegalArgumentException e) {
-            logger.warning("Ошибка покупки: " + e.getMessage());
+            logger.log(Level.WARNING,"Ошибка покупки: {}", e.getMessage());
         }
         logger.info("%n");
     }
